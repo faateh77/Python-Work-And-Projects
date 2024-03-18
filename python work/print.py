200f6f973ca60a4f8021bce5bbef2efa79c30eb8
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-18 18:06:25 +0500

    Add files via upload

---
+++ python work/print.py
@@ -0,0 +1,9 @@
+# name = 'tim'
+# age = 23
+# print(name+ 'is a boy')
+# print('my name is', age)
+
+print("hello world")
+print("happy coding")
+print("my name is faateh and my age is",100)
+print("my name is" + 'faateh')
