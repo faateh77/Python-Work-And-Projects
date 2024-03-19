5e587c47d4a84377084e33b4036682db7002dac4
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-19 14:47:07 +0500

    Add files via upload

---
+++ python work/numbers.py
@@ -0,0 +1,23 @@
+# number = 79
+# print(number)
+# print(78)
+number = 55
+#converting number  into string
+number2 = str(number)
+
+#checking the type of the number
+
+# print(type(number2))
+
+print("the number is"+ number2)
+
+#converting number into positive
+print(abs(-5))
+
+#determing whether number is greater or lesser
+print(max(11,2))
+print(min(2,3,43,6))
+#rounding off the number
+print(round(3.5))
+#converting the number into binary string form
+print(bin(334))
