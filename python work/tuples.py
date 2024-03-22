e7c536a2fcbbd8620c9ceaf30fdabf0a142fb96c
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-22 05:38:01 +0500

    Add files via upload

---
+++ python work/tuples.py
@@ -0,0 +1,21 @@
+#note: tuples are immutable
+three_number = (1,34,56)
+# print(three_number[0])
+#you can't declare any element when added once
+# three_number[1] = 23
+
+# print(three_number) 
+
+#Determining length of tuple
+# print(len(three_number))
+
+#determining its type whether its tuple or not
+# print(type(three_number))
+#we can make different tuples of different data type
+
+strings = ("apple","banana","mango")
+boo = (True,False)
+
+
+print(boo)
+print(type(strings[0]))
