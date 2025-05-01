fa041ae270da804a6422183b8b7505483e9dd4c8
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2025-05-01 18:31:10 +0500

    Add files via upload

---
+++ python work/stringfunctions.py
@@ -0,0 +1,35 @@
+w = "WelcometoPython34543coding "
+
+#lower
+l = w.lower()
+print(l)
+
+#upper
+u = w.upper()
+print(u)
+
+#title
+t = w.title()
+print(t)
+
+#Capitalize
+c = w.capitalize()
+print(c)
+
+#find
+f = w.find('c',4)
+print(f)
+
+#index
+i = w.index('e',4)
+print(i)
+
+#isalpha
+print(w.isalpha())
+
+#isdigit
+print(w.isdigit())
+
+#isalphanum
+print(w.isalnum())
+
