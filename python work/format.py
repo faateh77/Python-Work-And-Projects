c0012806c40aa94dad4af2f59fe042bc7c2bec84
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2025-05-04 06:23:39 +0500

    Add files via upload

---
+++ python work/format.py
@@ -0,0 +1,11 @@
+#w = "welcome {} to {} python coding".format("hello",20)
+#print(w)
+
+#w = "welcome {1} to {0} python coding".format("hello",20)
+#print(w)
+
+# w = "hello {fname} {lname} how are you?".format(fname = "Asif",lname= "Khan")
+# print(w)
+
+w = "welcome {a:<10} to {b:^12} python coding".format(a=10,b=20)
+print(w)
