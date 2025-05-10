33bed2aaf6b6b5b5ab03912f8a545329dcaf4fa4
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2025-05-11 02:14:34 +0500

    Add files via upload

---
+++ python work/lists.py
@@ -0,0 +1,14 @@
+#lists are mutable
+l = [1,2,3,4,5,6,7,8,9] 
+#slicing the list
+# print(l[3:5])  #first parameter starting point second parameter is from where we want go in the list
+
+# print(l[1: :2])
+
+l2 = [10,20,30,40,"Hello"]
+# print(l2[3:]) #first parameter is starting point and second one is till the end
+
+#slicing in reverse
+# print(l2[-1::-2])
+
+print(l2[-1::-1])
